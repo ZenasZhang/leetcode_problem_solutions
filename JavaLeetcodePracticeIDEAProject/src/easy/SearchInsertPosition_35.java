f829602552c803b3ff439b378519a7d4232894ec
ZenasZhang <zenaszhang@foxmail.com>
2019-12-22 11:26:27 +0800

    35 pass

--- JavaLeetcodePracticeIDEAProject/src/easy/SearchInsertPosition_35.java
+++ JavaLeetcodePracticeIDEAProject/src/easy/SearchInsertPosition_35.java
@@ -22,8 +22,8 @@ package easy;
 public class SearchInsertPosition_35 {
 
     public static void main(String[] args) {
-        int[] nums = new int[]{1,3,5,6};
-        System.out.println(searchInsert(nums,7));
+        int[] nums = {1,3,5,6,7,8,9,10,11};
+        System.out.println(searchInsert2(nums,2));
     }
 
     public static int searchInsert(int[] nums, int target) {
@@ -41,4 +41,21 @@ public class SearchInsertPosition_35 {
      * 执行用时 : 0 ms, 在所有 java 提交中击败了100.00%的用户
      * 内存消耗 :38.8 MB, 在所有 java 提交中击败了59.59%的用户
      */
+
+    // 尝试二分法
+    public static int searchInsert2(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+    // 上为二分法模板，要记牢。
 }
